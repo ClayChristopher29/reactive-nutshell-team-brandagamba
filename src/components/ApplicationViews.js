e26dfd322302f14788269ce47afa65c072f3977e
sydneywait <sydneywait@gmail.com>
2019-03-18 11:11:34 -0400

    added component mounting api calls in application views

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,47 @@
 import { Route, Redirect } from "react-router-dom";
 import React, { Component } from "react";
+// Comment or uncomment your import as needed
+// import UserAPIManager from "./users/UserManager"
+// import EventAPIManager from "./events/EventManager"
+// import NewsAPIManager from "./news/NewsManager"
+// import MessageAPIManager from "./messages/MessageManager"
+// import FriendAPIManager from "./friends/FriendManager"
 
 export default class ApplicationViews extends Component {
+
+
+  state = {
+    users: [],
+    events: [],
+    news: [],
+    messages: [],
+    friends: [],
+
+  }
+
+  // Check if credentials are in local storage
+  // isAuthenticated = () => sessionStorage.getItem("credentials") !== null
+
+  componentDidMount() {
+
+    const newState = {}
+
+    // Get all info from the API and set state
+    // comment or uncomment your module as needed
+
+    //        UserAPIManager.getAllUsers()
+    //             .then(users => newState.users = users)
+    //             .then(EventAPIManager.getAllEvents)
+    //             .then(events => newState.events = events)
+    //             .then(NewsAPIManager.getAllNews)
+    //             .then(news => newState.news = news)
+    //             .then(MessageAPIManager.getAllMessages)
+    //             .then(messages => newState.messages = messages)
+    //             .then(FriendAPIManager.getAllFriends)
+    //             .then(friends => newState.friends = friends)
+    //             .then(() => this.setState(newState))
+
+  }
 
   render() {
     return (
@@ -34,7 +74,7 @@ export default class ApplicationViews extends Component {
             // Remove null and return the component which will show the user's tasks
           }}
         />
-        
+
       </React.Fragment>
     );
   }
